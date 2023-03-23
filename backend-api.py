ac884c768fc8a20109079c394986b33700e40dc6
Classixz <classixz@undeaduprising.net>
2023-03-23 18:32:05 +0100

    Added the flask api

---
+++ backend-api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask
+from flask import request
+import requests
+import nmap
+import whois
+app = Flask(__name__)
+
+
+
+@app.route('/web-scanner')
+def web_scanner():
+    url = request.args.get('url')
+    r = requests.get(url)
+    return json.dumps(dict(r.headers), indent=4, sort_keys=True)
+
+@app.route('/web-whois')
+def web_whois():
+    url = request.args.get('url')
+    w = dict(whois.whois(url))
+    return json.dumps(w, indent=4, sort_keys=True, default=str)
+
+@app.route('/port-scanner')
+def port_scanner():
+    url = request.args.get('ip')
+    nm = nmap.PortScanner()
+    res = nm.scan(url, '22-443')
+    return json.dumps({'res': dict(res)})
+
+app.run()

bf51eea93e932225fed580e52a7a3237be0ecfdc
Classixz <classixz@undeaduprising.net>
2023-03-27 16:07:27 +0200

    Added some more items to the FAQ

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -22,27 +22,33 @@ export default async function IndexPage() {
             Frequently asked questions
           </h1>
           <p className="max-w-[700px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
-            If something is not answered here, please contact us.
+            If something is not answered here, please contact us via GitHub.
           </p>
         </div>
         <div className="flex gap-4">
           <Accordion type="single" collapsible className="w-full">
             <AccordionItem value="item-1">
-              <AccordionTrigger>Is this leagal?</AccordionTrigger>
+              <AccordionTrigger>Is this legal?</AccordionTrigger>
               <AccordionContent>
-                Yes. It should be
+                Yes. It should be, but we are not responsible for any legal issues you might encounter. Please only scan your own network, or ask permission from the owner. This tool is not intended for malicious purposes, and was created for educational purposes.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
               <AccordionTrigger>Where can I find the source code?</AccordionTrigger>
               <AccordionContent>
-                You can find it on our github, click the github icon in the top right corner.
+                You can find it on our GitHub, click the GitHub icon in the top right corner.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
               <AccordionTrigger>What is the purpose of this project?</AccordionTrigger>
               <AccordionContent>
-                The purpose is to give the user a better understanding of their network. Scan for issues and misconfigurations.
+                The purpose is to give the user a better understanding of their network. Scan for issues and misconfigurations. This project was created for educational purposes, and is not intended for malicious purposes. The project was created by students at the University of Skövde in the PICS Masters Program.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger>I found an issue, how can I report it?</AccordionTrigger>
+              <AccordionContent>
+                Please open a new issue on our GitHub, so we can fix it.
               </AccordionContent>
             </AccordionItem>
           </Accordion>

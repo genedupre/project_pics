7f8dd4af0eb92340a5cfa3ea10e9efcc4bd6b0ad
Classixz <classixz@undeaduprising.net>
2023-04-07 20:51:58 +0200

    Updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,47 @@
-"use client";
-import { Inter } from 'next/font/google';
+"use client"
+
+import { useEffect, useState } from "react"
+import { Inter } from "next/font/google"
 import { ThemeProvider } from "next-themes"
-import { useEffect, useState } from "react";
-import "nextjs-components/src/styles/globals.css";
+
+import "nextjs-components/src/styles/globals.css"
 import "@/styles/globals.css"
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
+    subsets: ["latin"],
+    display: "swap",
 })
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  const [mounted, setMounted] = useState(false);
+    const [mounted, setMounted] = useState(false)
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+    useEffect(() => {
+        setMounted(true)
+    }, [])
 
-  return (
-    <html lang="en" className={inter.className}>
-      {/*
+    return (
+        <html lang="en" className={inter.className}>
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body className="min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
-        {mounted && (
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
-          </ThemeProvider>
-        )}
-      </body>
-    </html>
-  );
+            <head />
+            <body className="min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
+                {mounted && (
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                    >
+                        {children}
+                    </ThemeProvider>
+                )}
+            </body>
+        </html>
+    )
 }

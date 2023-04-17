e5e8bf9938397f23de4d21ffcc7c6b234e89a893
Classixz <classixz@undeaduprising.net>
2023-04-17 09:10:11 +0200

    Added support to scan certificates

--- backend-api.py
+++ backend-api.py
@@ -6,6 +6,9 @@ from flask import request
 import requests
 import nmap
 import whois
+import ssl
+import socket
+from urllib.parse import urlparse
 app = Flask(__name__)
 
 @app.route('/webscan')
@@ -27,4 +30,13 @@ def port_scanner():
     res = nm.scan(url, '22-443')
     return json.dumps({'res': dict(res)})
 
+
+@app.route('/cert-info')
+def cert_info():
+    url = urlparse(request.args.get('url'))
+    ctx = ssl.create_default_context()
+    with socket.create_connection((url.netloc, 443)) as sock:
+        with ctx.wrap_socket(sock, server_hostname=url.netloc) as ssock:
+            cert = ssock.getpeercert()
+            return json.dumps(cert)
 app.run()

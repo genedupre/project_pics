f448504cc2fed18b1397e0b379b009bc56d1d022
Classixz <classixz@undeaduprising.net>
2023-05-08 14:38:59 +0200

    skeleton updates

--- components/server/latest-cves-skeleton.tsx
+++ components/server/latest-cves-skeleton.tsx
@@ -1,58 +1,64 @@
+import Link from "next/link"
+
 import { Skeleton } from "@/components/ui/skeleton"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 export function LatestCVEsSkeleton() {
     return (
-        <>
-            <div className="w-full">
-                <h2 className="sm:text-1xl text-2xl font-bold leading-tight tracking-tighter md:text-2xl lg:text-3xl">
-                    Newest CVEs
-                </h2>
-                <div className="mt-5 rounded-xl bg-slate-50 pt-0.5 dark:bg-slate-800/25">
-                    <div className=" overflow-none my-4 shadow-sm">
-                        <table className=" table-auto border-collapse text-sm">
-                            <thead>
-                                <tr>
-                                    <th className="hidden w-1/6 border-b p-4 pb-3 pl-8 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200 md:table-cell">
-                                        ID
-                                    </th>
-                                    <th className="hidden w-4/5 border-b p-4 pb-3 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200 md:table-cell">
-                                        Summary
-                                    </th>
-                                    <th className="w-1/8 hidden border-b p-4 pb-3 pr-8 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200 md:table-cell">
-                                        CWE
-                                    </th>
-                                    <th className="w-1/8 hidden border-b p-4 pb-3 pr-8 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200 md:table-cell"></th>
-                                </tr>
-                            </thead>
-                            <tbody
-                                className="bg-white dark:bg-slate-800"
-                                key="newest-cve"
-                            >
-                                {Array.from(Array(5).keys()).map((i) => (
-                                    <tr key={i}>
-                                        <td className="border-b border-slate-100 p-4 pl-8 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                            <Skeleton className="h-5 w-3/5" />
-                                        </td>
-                                        <td className="border-b border-slate-100 p-4 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                            <Skeleton className="h-5 w-full" />
-                                        </td>
-                                        <td className="border-b border-slate-100 p-4 pr-8 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                            <Skeleton className="h-5 w-3/5" />
-                                        </td>
-                                        <td className="border-b border-slate-100 p-4 pr-8 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                            <Skeleton className="h-5 w-3/5" />
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                    <p className="mb-4 pb-4 text-center text-xs font-semibold text-slate-900 dark:text-slate-400">
-                        Data provided by cve-search
-                    </p>
-                </div>
-                <div className="pointer-events-none absolute inset-0 rounded-xl border border-black/5 dark:border-white/5"></div>
+        <div className="w-full">
+            <h2 className="sm:text-1xl text-2xl font-bold leading-tight tracking-tighter md:text-2xl lg:text-3xl">
+                Latest CVEs
+            </h2>
+            <h3 className="text-slate-700 dark:text-slate-400">
+                The latest CVEs are not related to scan results, but are pulled
+                from the NIST database to provide a quick reference to the
+                latest CVEs.
+            </h3>
+            <div className="mt-5 rounded-xl bg-slate-50 pt-0.5 dark:bg-slate-800/25">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[150px] border-b p-4 py-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                                ID
+                            </TableHead>
+                            <TableHead className="border-b p-4 py-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                                Description
+                            </TableHead>
+                            <TableHead className="w-[50px] border-b text-right dark:border-slate-600"></TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {Array.from(Array(5).keys()).map((i) => (
+                            <TableRow key={i}>
+                                <TableCell className="border-b border-slate-100 font-medium text-slate-500 dark:border-slate-700 dark:text-slate-400">
+                                    <Skeleton className="h-5" />
+                                </TableCell>
+                                <TableCell className="border-b border-slate-100 dark:border-slate-700">
+                                    <Skeleton className="h-5" />
+                                </TableCell>
+                                <TableCell className="border-b border-slate-100 text-right text-slate-500 dark:border-slate-700 dark:text-slate-400">
+                                    <Skeleton className="h-5" />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    <TableCaption className="pb-4 text-center text-xs font-semibold text-slate-500 dark:border-slate-700 dark:text-slate-400">
+                        Data provided by{" "}
+                        <Link href="https://nvd.nist.gov/" target="_blank">
+                            National Institute of Standards and Technology
+                            (NIST)
+                        </Link>
+                    </TableCaption>
+                </Table>
             </div>
-        </>
+        </div>
     )
 }

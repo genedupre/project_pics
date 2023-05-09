499b3afa879b280da699402ba21dd2a4bf5ea19e
Classixz <classixz@undeaduprising.net>
2023-05-09 10:15:54 +0200

    Scan skeleton now uses new table

--- components/client/scan-skeleton.tsx
+++ components/client/scan-skeleton.tsx
@@ -1,5 +1,14 @@
 import { Separator } from "@/components/ui/separator"
 import { Skeleton } from "@/components/ui/skeleton"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 export function ScanSkeleton() {
     return (
@@ -8,46 +17,34 @@ export function ScanSkeleton() {
                 <h2 className="sm:text-1xl mt-4 text-2xl font-bold leading-tight tracking-tighter md:text-2xl lg:text-3xl">
                     Scan Result
                 </h2>
-                <div className="not-prose relative mt-5 hidden overflow-hidden rounded-xl bg-slate-50 dark:bg-slate-800/25 md:block">
-                    <div
-                        style={{ backgroundPosition: "10px 10px" }}
-                        className="bg-grid-slate-100 dark:bg-grid-slate-700/25 absolute inset-0 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
-                    ></div>
-                    <div className="relative overflow-auto rounded-xl">
-                        <div className="my-4 overflow-hidden shadow-sm">
-                            <table className="w-full table-auto border-collapse text-sm">
-                                <thead>
-                                    <tr>
-                                        <th className="w-1/6 border-b p-4 pb-3 pl-8 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
-                                            Key
-                                        </th>
-                                        <th className="w-4/6 border-b p-4 pb-3 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
-                                            Value
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody
-                                    className="bg-white dark:bg-slate-800"
-                                    key="scan-result"
-                                >
-                                    {Array.from(Array(5).keys()).map((i) => (
-                                        <tr key={i}>
-                                            <td className="border-b border-slate-100 p-4 pl-8 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                                <Skeleton className="h-5 w-3/5" />
-                                            </td>
-                                            <td className="border-b border-slate-100 p-4 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                                <Skeleton className="h-5 w-5/6" />
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                        <p className="mb-3 text-center text-xs font-semibold text-slate-900 dark:text-slate-400">
-                            Loading...
-                        </p>
-                    </div>
-                    <div className="pointer-events-none absolute inset-0 rounded-xl border border-black/5 dark:border-white/5"></div>
+                <div className="mt-5 rounded-xl bg-slate-50 pt-0.5 dark:bg-slate-800/25">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[250px] border-b p-4 py-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                                    Key
+                                </TableHead>
+                                <TableHead className="border-b p-4 py-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                                    Value
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {Array.from(Array(5).keys()).map((i) => (
+                                <TableRow key={i}>
+                                    <TableCell className="border-b border-slate-100 font-medium text-slate-500 dark:border-slate-700 dark:text-slate-400">
+                                        <Skeleton className="h-5" />
+                                    </TableCell>
+                                    <TableCell className="border-b border-slate-100 dark:border-slate-700">
+                                        <Skeleton className="h-5" />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableCaption className="pb-4 text-center text-xs font-semibold text-slate-500 dark:border-slate-700 dark:text-slate-400">
+                            Loading results...
+                        </TableCaption>
+                    </Table>
                 </div>
             </div>
             <Separator className="hidden md:block" />

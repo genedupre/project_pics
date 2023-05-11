760229d020be58d655129226d629b6c0cddf90ed
Classixz <classixz@undeaduprising.net>
2023-05-11 16:58:45 +0200

    Scans now use their own components for less clutter

--- components/client/scan.tsx
+++ components/client/scan.tsx
@@ -5,7 +5,10 @@ import { AlertCircle, Info } from "lucide-react"
 
 import { ScanSkeleton } from "@/components/client/scan-skeleton"
 import { Icons } from "@/components/icons"
-import CertificateData from "@/components/scanning/CertificateData"
+import CertScan from "@/components/scanning/CertScan"
+import DNSScan from "@/components/scanning/DNSScan"
+import WebScan from "@/components/scanning/WebScan"
+import WhoisScan from "@/components/scanning/WhoisScan"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import {
     AlertDialog,
@@ -218,110 +221,39 @@ export function ScanForm() {
                         <h2 className="sm:text-1xl mt-4 text-2xl font-bold leading-tight tracking-tighter md:text-2xl lg:text-3xl">
                             Scan Result
                         </h2>
-                        <div className="not-prose relative mt-5 hidden overflow-hidden rounded-xl bg-slate-50 dark:bg-slate-800/25 md:block">
-                            <div
-                                style={{ backgroundPosition: "10px 10px" }}
-                                className="bg-grid-slate-100 dark:bg-grid-slate-700/25 absolute inset-0 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
+                        {tool === "webscan" && <WebScan data={result} />}
+                        {tool === "cert-info" && <CertScan data={result} />}
+                        {tool === "web-whois" && <WhoisScan data={result} />}
+                        {tool === "dns-records" && <DNSScan data={result} />}
                             ></div>
-                            <div className="relative overflow-auto rounded-xl">
-                                <div className="my-4 overflow-hidden shadow-sm">
-                                    {tool != "cert-info" ? (
-                                        <table className="w-full table-auto border-collapse text-sm">
-                                            <thead>
-                                                <tr>
-                                                    <th className="w-1/6 border-b p-4 pb-3 pl-8 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
-                                                        Key
-                                                    </th>
-                                                    <th className="w-4/6 border-b p-4 pb-3 pt-0 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
-                                                        Value
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody
-                                                className="bg-white dark:bg-slate-800"
-                                                key="scan-result"
-                                            >
-                                                {Object.keys(result).map(
-                                                    (key): React.ReactNode => (
-                                                        <tr>
-                                                            <td className="border-b border-slate-100 p-4 pl-8 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                                                {key}
-                                                            </td>
-                                                            <td className="border-b border-slate-100 p-4 text-slate-500 dark:border-slate-700 dark:text-slate-400">
-                                                                {typeof result[
-                                                                    key
-                                                                ] ===
-                                                                "object" ? (
-                                                                    <>
-                                                                        {result[
-                                                                            key
-                                                                        ]?.map(
-                                                                            (
-                                                                                x: string
-                                                                            ) => (
-                                                                                <div
-                                                                                    key={String(
-                                                                                        x
-                                                                                    )}
-                                                                                >
-                                                                                    {
-                                                                                        x
-                                                                                    }
-                                                                                </div>
-                                                                            )
-                                                                        )}
-                                                                    </>
-                                                                ) : (
-                                                                    <>
-                                                                        {
-                                                                            result[
-                                                                                key
-                                                                            ]
-                                                                        }
-                                                                    </>
-                                                                )}
-                                                            </td>
-                                                        </tr>
-                                                    )
-                                                )}
-                                            </tbody>
-                                        </table>
-                                    ) : (
-                                        <CertificateData data={result} />
-                                    )}
-                                </div>
-                                <p className="mb-3 text-center text-xs font-semibold text-slate-900 dark:text-slate-400">
-                                    You did a {tool} the following website:{" "}
-                                    <b>{server}</b>
-                                </p>
-                            </div>
-                            <div className="pointer-events-none absolute inset-0 rounded-xl border border-black/5 dark:border-white/5"></div>
-                        </div>
                     </div>
+                    {tool !== "portscan" && (
+                        <>
+                            <Separator className="hidden md:block" />
 
-                    <Separator className="hidden md:block" />
-
-                    <div className="not-prose relative mb-4 mt-8 overflow-hidden rounded-xl border bg-slate-50 dark:border-slate-700 dark:bg-slate-800/25">
-                        <div className="flex items-center justify-between border-b px-3 py-2 dark:border-gray-600">
-                            <div className="flex flex-wrap items-center divide-gray-200 dark:divide-gray-600 sm:divide-x">
-                                <div className="flex items-center space-x-1 sm:pr-4">
-                                    JSON Output
+                            <div className="not-prose relative mb-4 mt-8 overflow-hidden rounded-xl border bg-slate-50 dark:border-slate-700 dark:bg-slate-800/25">
+                                <div className="flex items-center justify-between border-b px-3 py-2 dark:border-gray-600">
+                                    <div className="flex flex-wrap items-center divide-gray-200 dark:divide-gray-600 sm:divide-x">
+                                        <div className="flex items-center space-x-1 sm:pr-4">
+                                            JSON Output
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="rounded-b-lg bg-white px-4 py-2 dark:bg-gray-800">
+                                    <label htmlFor="json" className="sr-only">
+                                        JSON Output
+                                    </label>
+                                    <textarea
+                                        id="json"
+                                        rows={12}
+                                        className="block w-full border-0 bg-white px-0 text-sm text-gray-800 focus:ring-0 dark:bg-slate-800 dark:text-white dark:placeholder:text-gray-400"
+                                        value={JSON.stringify(result, null, 2)}
+                                        readOnly
+                                    />
                                 </div>
                             </div>
-                        </div>
-                        <div className="rounded-b-lg bg-white px-4 py-2 dark:bg-gray-800">
-                            <label htmlFor="json" className="sr-only">
-                                JSON Output
-                            </label>
-                            <textarea
-                                id="json"
-                                rows={12}
-                                className="block w-full border-0 bg-white px-0 text-sm text-gray-800 focus:ring-0 dark:bg-slate-800 dark:text-white dark:placeholder:text-gray-400"
-                                value={JSON.stringify(result, null, 2)}
-                                readOnly
-                            />
-                        </div>
-                    </div>
+                        </>
+                    )}
                 </>
             )}
         </>

6f9e73a4cd245281c1b2081f0d8cbbae44346b8b
Classixz <classixz@undeaduprising.net>
2023-05-25 11:19:29 +0200

    Finished checks for making sure that user only scans verified domains

--- app/api/scan/route.ts
+++ app/api/scan/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server"
+import { cookies } from "next/headers"
+import { db } from "@/lib/db"
 
 export async function POST(request: Request) {
     const res = await request.json()
@@ -17,7 +19,7 @@ export async function POST(request: Request) {
     }
 
     // Check if the protocol is none, if so make sure that the server is a valid IP address
-    if (protocol == "none" && !/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(server)) {
+    if (protocol == "none" && !/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(server) && tool != "portscan") {
         return NextResponse.json({
             error: "A protocol was not specified, but you are not scanning an IP address, please select a protocol.",
         })
@@ -43,19 +45,63 @@ export async function POST(request: Request) {
     }
 
     // Check if the tool is valid, if not, return an error
-    if (!["webscan", "web-whois", "cert-info", "dns-records"].includes(tool)) {
+    if (!["webscan", "web-whois", "cert-info", "dns-records", "portscan"].includes(tool)) {
         return NextResponse.json({ error: "Invalid tool" })
     }
 
     // Log the request
     console.log(`Got ${tool} request on server at ${server}`)
 
-    // Send a get request to the server
-    const response = await fetch(
-        `${process.env.SCAN_API_ENDPOINT}/${tool}?url=${server}`
-    )
-    const data = await response.json()
+    if(tool == "portscan") {
+        if(server == "scanme.nmap.org") { // This is a special case, because it is a domain that is not verified but is allowed
+            return NextResponse.json({
+                domain: server,
+            })
+        }
 
-    // Return the data
-    return NextResponse.json(data)
+        const cookieStore = cookies()
+        const verified_domains = cookieStore.get("verified_domains")
+
+        const domainsVerified = verified_domains?.value.split(",")
+
+        if(domainsVerified == null) {
+            return NextResponse.json({ error: "You have not verified any domains yet." })
+        }
+
+        const allowed_domains = await db.domains.findMany({
+            where: {
+                key: {
+                    in: domainsVerified,
+                },
+            },
+            select: {
+                id: true,
+                domain: true,
+                key: true,
+                createdAt: true,
+            },
+        })
+
+        const allowed_domains_array = allowed_domains.map((domain) => domain.domain)
+
+        if(!allowed_domains_array.includes(server)) {
+            return NextResponse.json({ error: "You have not verified this domain yet." })
+        }
+
+        // Return the data
+        return NextResponse.json({
+            domain: server,
+        })
+    
+    } else {
+        // Send a get request to the server
+        const response = await fetch(
+            `${process.env.SCAN_API_ENDPOINT}/${tool}?url=${server}`
+        )
+        const data = await response.json()
+
+        // Return the data
+        return NextResponse.json(data)
+    }
+
 }
